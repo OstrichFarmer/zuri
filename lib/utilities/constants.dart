c81332971a1ff02bee36b4575fdd5d183c3e76f6
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-11-06 06:37:07 +0100

    created the dimensions file

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zuri/utilities/dimensions.dart';
 
 final kHeadingStyling = TextStyle(
   fontSize: Dimensions.height30,
---
+++ lib/utilities/dimensions.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+
+// screenHeight = 797.7 screenWidth = 411.4
+class Dimensions {
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+// Dynamic heights
+  static double height343 = screenHeight / 2.32;
+  static double height60 = screenHeight / 13.29;
+  static double height25 = screenHeight / 31.9;
+  static double height15 = screenHeight / 53.18;
+  static double height16 = screenHeight / 49.85;
+  static double height10 = screenHeight / 79.77;
+  static double height20 = screenHeight / 39.88;
+  static double height30 = screenHeight / 26.59;
+  static double height5 = screenHeight / 159.54;
+  static double height50 = screenHeight / 15.95;
+
+  // Dynamic fonts
+  static double font16 = screenHeight / 49.85;
+  static double font18 = screenHeight / 44.31;
+  static double font24 = screenHeight / 33.23;
+  static double font20 = screenHeight / 39.88;
+}
