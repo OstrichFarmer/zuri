f8478797f1b50f8063a6217fd07f1f54011ccd6d
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-11-06 06:42:01 +0100

    created a custom textfield

---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../utilities/colors.dart';
+import '../utilities/dimensions.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  final Function onchanged;
+  const CustomTextField({
+    Key? key,
+    required this.hintText,
+    required this.onchanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onChanged: onchanged(),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        hintText: hintText,
+        hintStyle: TextStyle(
+          fontSize: Dimensions.font18,
+          fontWeight: FontWeight.w400,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(Dimensions.height5),
+          borderSide: const BorderSide(
+            color: AppColors.royalOrange,
+          ),
+        ),
+        contentPadding: EdgeInsets.all(Dimensions.height10),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(Dimensions.height5),
+        ),
+      ),
+    );
+  }
+}
