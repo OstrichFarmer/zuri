fce93566867d046399a6418ea33957c124ccc645
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-11-09 02:35:56 +0100

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+// ignore: depend_on_referenced_packages
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:zuri/screens/splashscreen/splashscreen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
