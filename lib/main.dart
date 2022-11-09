876927302f9c29b8ad21d07dfb5321a22f7998ea
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-11-09 06:39:29 +0100

    added firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:zuri/screens/splashscreen/splashscreen.dart';
+import 'firebase_options.dart';
+
+// ...
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Zuri App',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
--- lib/screens/onboarding/login.dart
+++ lib/screens/onboarding/login.dart
@@ -30,69 +30,67 @@ class _LoginScreenState extends State<LoginScreen> {
                   Dimensions.height60,
                   Dimensions.height16,
                   Dimensions.height20),
-              child: Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Sign In',
-                      style: kHeadingStyling,
-                    ),
-                    SizedBox(
-                      height: Dimensions.height10,
-                    ),
-                    Text(
-                      'Log in to your account to continue ',
-                      style: kHeading2styling,
-                    ),
-                    SizedBox(
-                      height: Dimensions.height15,
-                    ),
-                    const Text('Email'),
-                    SizedBox(
-                      height: Dimensions.height5,
-                    ),
-                    CustomTextField(
-                      hintText: 'Enter email address ',
-                      onchanged: () {},
-                    ),
-                    SizedBox(
-                      height: Dimensions.height25,
-                    ),
-                    const Text('Password'),
-                    SizedBox(
-                      height: Dimensions.height5,
-                    ),
-                    CustomPasswordTextBox((_) {}, 'Enter password'),
-                    SizedBox(
-                      height: Dimensions.height16,
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: () {
-                          Get.to(() => const ForgotPasswordScreen());
-                        },
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyle(
-                              fontSize: Dimensions.height16,
-                              color: AppColors.royalOrange),
-                        ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sign In',
+                    style: kHeadingStyling,
+                  ),
+                  SizedBox(
+                    height: Dimensions.height10,
+                  ),
+                  Text(
+                    'Log in to your account to continue ',
+                    style: kHeading2styling,
+                  ),
+                  SizedBox(
+                    height: Dimensions.height15,
+                  ),
+                  const Text('Email'),
+                  SizedBox(
+                    height: Dimensions.height5,
+                  ),
+                  CustomTextField(
+                    hintText: 'Enter email address ',
+                    onchanged: () {},
+                  ),
+                  SizedBox(
+                    height: Dimensions.height25,
+                  ),
+                  const Text('Password'),
+                  SizedBox(
+                    height: Dimensions.height5,
+                  ),
+                  CustomPasswordTextBox((_) {}, 'Enter password'),
+                  SizedBox(
+                    height: Dimensions.height16,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        Get.to(() => const ForgotPasswordScreen());
+                      },
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                            fontSize: Dimensions.height16,
+                            color: AppColors.royalOrange),
                       ),
                     ),
-                    SizedBox(
-                      height: Dimensions.height50,
-                    ),
-                    Center(
-                      child: CustomButton(
-                          title: 'Sign In',
-                          ontap: () {
-                            Get.to(() => const HomeScreen());
-                          }),
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: Dimensions.height50,
+                  ),
+                  Center(
+                    child: CustomButton(
+                        title: 'Sign In',
+                        ontap: () {
+                          Get.to(() => const HomeScreen());
+                        }),
+                  ),
+                ],
               ),
             ),
           ),
