712ea0f41a31b7440d111d4bc950c07386492fd4
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-11-09 09:43:09 +0100

    finished the authentication repository

---
+++ lib/repository/authentication_repository.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'package:zuri/screens/onboarding/home.dart';
+import 'package:zuri/screens/onboarding/signup_screen.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+  //Variable
+
+  final _auth = FirebaseAuth.instance;
+  late final Rx<User?> firebaseUser;
+
+  @override
+  void onReady() {
+    firebaseUser = Rx<User?>(_auth.currentUser);
+    firebaseUser.bindStream(_auth.userChanges());
+    ever(firebaseUser, _setInitialScreen);
+  }
+
+  _setInitialScreen(User? user) {
+    user == null
+        ? Get.offAll(() => const SignUpScreen())
+        : Get.offAll(() => const HomeScreen());
+  }
+
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+    } catch (_) {}
+  }
+
+  Future<void> loginUserWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+    } catch (_) {}
+  }
+
+  Future<void> SignOut() async => _auth.signOut();
+}
